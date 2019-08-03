da0e56fb78dc63698725448ae2ec29f8c496c7c8
gaurav_ranpara <ranparagaurav98@gmail.com>
2019-08-03 12:46:58 +0530

    Changes in Screeners Page

--- resources/views/front/screeners/view.blade.php
+++ resources/views/front/screeners/view.blade.php
@@ -5,246 +5,49 @@
 
 
 @section('content')
-<!-- <div class="page-title sp" style="background-image: url(assets/img/page-title.jpg)">
-    <div class="container text-center">
-        <h2>Screeners</h2>
-    </div>
-</div> -->
 
-
-<!-- <div class="service-area spt">
-    <div class="container bg1" data-padding="40px">
-        <div class="row">
-            <div class="col-lg-4 col-md-6 single-service-3">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-film"></i>
-                        </div>
-                        <h4>Video Making</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service-3">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-camera"></i>
-                        </div>
-                        <h4>Photography</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service-3">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-music"></i>
-                        </div>
-                        <h4>Music Composing</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service-3">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-bullhorn"></i>
-                        </div>
-                        <h4>Product Marketing</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service-3">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-magic"></i>
-                        </div>
-                        <h4>Video Editing</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service-3">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-bar-chart"></i>
-                        </div>
-                        <h4>Presentation</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->   
 <div class="service-area sp">
     <div class="container">
         <div class="section-title" data-margin="0 0 40px">
             <h2>Screeners</h2>
-            <!-- <p>Malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p> -->
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-6 single-service-2">
+            <div class="col-lg-6 col-md-6 single-service-2 bs-callout">
                 <div class="card">
                     <div class="card-header">
+                        <h2>Fundamental Scans</h2>
                     </div>
                     <div class="card-body">
-                        <ul style="list-style: none;text-align: left;">
-                            <li><a href="#" id="wipro_chart">Wipro</a></li>
+                        <ul class="list-group striped" style="list-style: none;text-align: left;">
+                            <li class="list-group-item"><a href="#" id="wipro_chart">Wipro</a></li>
+                            <li class="list-group-item" style="background-color: #eeeeee;"><a href="#">Profit Jump by 200%</a></li>
+                            <li class="list-group-item"><a href="#">Swing Trade Scanner</a></li>
+                            <li class="list-group-item" style="background-color: #eeeeee;"><a href="#">Low Debt Companies</a></li>
+                            <li class="list-group-item"><a href="#">Mid Cap Stocks</a></li>
+                            <li class="list-group-item" style="background-color: #eeeeee;"><a href="#">Small Cap Stocks</a></li>
                         </ul>
+                    </div>
+                </div>
+            </div>
 
+
+            <div class="col-lg-6 col-md-6 single-service-2">
+                <div class="card">
+                    <div class="card-header" style="text-align: left;">
+                        <h4 id="stock_name"></h4>
+                    </div>
+                    <div class="card-body">
                         <div id="chart_div">
                         </div>
                     </div>
                 </div>
-                <!-- <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-film"></i>
-                        </div>
-                        <h4>Video Making</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div> -->
-            </div>
-            <!-- <div class="col-lg-4 col-md-6 single-service-2">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-camera"></i>
-                        </div>
-                        <h4>Photography</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service-2">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-music"></i>
-                        </div>
-                        <h4>Music Composing</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
-            </div> -->
-        </div>
-    </div>
-</div>    
-<!-- <div class="service-area bg2 sp">
-    <div class="container">
-        <div class="section-title">
-            <h2>Our Service</h2>
-            <p>Malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-6 single-service">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-film"></i>
-                        </div>
-                        <h4>Video Making</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-camera"></i>
-                        </div>
-                        <h4>Photography</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-music"></i>
-                        </div>
-                        <h4>Music Composing</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-bullhorn"></i>
-                        </div>
-                        <h4>Product Marketing</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-magic"></i>
-                        </div>
-                        <h4>Video Editing</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 single-service">
-                <div class="inner">
-                    <div class="title">
-                        <div class="icon">
-                            <i class="fa fa-bar-chart"></i>
-                        </div>
-                        <h4>Presentation</h4>
-                    </div>
-                    <div class="content">
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames.</p>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
-</div> -->
+</div>
+
+
+<div style="display: none;" id="loader"><img src="{{asset('assets/img/Loading.gif')}}" style="position: absolute;top: 37%;left: 37%;"></div>    
 @endsection
 
 
@@ -261,6 +64,7 @@
         });
 
         $(document).on('click','#wipro_chart',function(){
+            $("#loader").css('display','');
             $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -268,6 +72,8 @@
                 url: '/screeners_get',
                 method: 'POST',
                 success:function(result){
+                    $("#loader").css('display','none');
+                    $("#stock_name").text('Wipro');
                     var result_Data = [];
                     $.each(result,function(key,value){
                         var tmp_new_date = value.Date.split(/[- :]/);
@@ -281,17 +87,12 @@
 
                     const json_data = JSON.stringify(result_Data);
 
-                    //var new_data_object = Object.assign({},result_Data);
-                    //var json_data = JSON.stringify(new_data_object);
-                    /*console.log(json_data);
-                    return false;*/
-
-
+                    
                     const chart = LightweightCharts.createChart(document.getElementById('chart_div'), {
-                        width: 600,
+                        width: 500,
                         height: 300,
                         layout: {
-                            backgroundColor: '#000000',
+                            backgroundColor: '#222538',
                             textColor: 'rgba(255, 255, 255, 0.9)',
                         },
                         grid: {
@@ -316,12 +117,12 @@
                     });
 
                     const candleSeries = chart.addCandlestickSeries({
-                      upColor: 'rgba(255, 144, 0, 1)',
-                      downColor: '#000',
-                      borderDownColor: 'rgba(255, 144, 0, 1)',
-                      borderUpColor: 'rgba(255, 144, 0, 1)',
-                      wickDownColor: 'rgba(255, 144, 0, 1)',
-                      wickUpColor: 'rgba(255, 144, 0, 1)',
+                        upColor: 'rgba(255, 144, 0, 1)',
+                        downColor: '#000',
+                        borderDownColor: 'rgba(255, 144, 0, 1)',
+                        borderUpColor: 'rgba(255, 144, 0, 1)',
+                        wickDownColor: 'rgba(255, 144, 0, 1)',
+                        wickUpColor: 'rgba(255, 144, 0, 1)',
                     });
 
                     candleSeries.setData(result_Data);

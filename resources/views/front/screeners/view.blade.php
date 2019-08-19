f93876178b210b5da6a9897a4c54716d63640e88
gaurav_ranpara <ranparagaurav98@gmail.com>
2019-08-19 22:43:04 +0530

    Change Design Of Screeners Page.

--- resources/views/front/screeners/view.blade.php
+++ resources/views/front/screeners/view.blade.php
@@ -21,29 +21,118 @@
             <div class="col-lg-4 col-md-4 single-service-2">
                 <div class="card border border-primary">
                     <div class="card-body">
-                        <h4 class="text-primary text-left">Fundamental Scans</h4>
+                        <h4 class="text-primary text-left">Top Loved</h4>
                         <ul class="list-group text-left">
-                            <!-- <li class="list-group-item"><a href="#" id="wipro_chart">Wipro</a></li> -->
-                            <li class="list-group-item list-group-item-light"><a href="/screeners/Wipro">Wipro</a></li>
-                            <li class="list-group-item list-group-item-dark"><a href="#">Profit Jump by 200%</a></li>
-                            <li class="list-group-item list-group-item-light"><a href="#">Swing Trade Scanner</a></li>
-                            <li class="list-group-item list-group-item-dark"><a href="#">Low Debt Companies</a></li>
-                            <li class="list-group-item list-group-item-light"><a href="#">Mid Cap Stocks</a></li>
-                            <li class="list-group-item list-group-item-dark"><a href="#">Small Cap Stocks</a></li>
+                            <!-- <li class="list-group-item list-group-item-light"><a href="/screeners/Wipro">Wipro</a></li> -->
+                            <li class="list-group-item list-group-item-light"><a href="#">15 Minute stock Breakouts</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Short term Breakouts</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Intraday buying seen in the Past 15 minutes</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Bulish for next day</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Supertrend positive breakout - 10 mins</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Volume shockers(stocks with rising volumes)</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Perfect sell(short)</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">NKs best buy stocks for intraday</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Potential breakouts</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Strong stocks</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Macd bearish or bullish crossover</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Daily rsi oversold/overbought scan</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
 
 
-            <div class="col-lg-6 col-md-6 single-service-2">
-                <div class="card">
-                    <div class="card-header" style="text-align: left;">
-                        <h4 id="stock_name"></h4>
-                    </div>
+            <div class="col-lg-4 col-md-4 single-service-2">
+                <div class="card border border-warning">
                     <div class="card-body">
-                        <div id="chart_div">
-                        </div>
+                        <h4 class="text-primary text-left">Candlestick Patterns Scan</h4>
+                        <ul class="list-group text-left">
+                            <li class="list-group-item list-group-item-light"><a href="#">15 Minute stock Breakouts</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Short term Breakouts</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Supertrend positive breakout - 10 mins</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Volume spike on 1 minute candle</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Top Shares for 2018 - stocks to invest in</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Morning star candlestick pattern</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Doji</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Evening star candlestick pattern</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Buy Entry intraday</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Positive hammer</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Sell entry intraday</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Breakout intraday</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Breakout days high - 5 mins</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-lg-4 col-md-4 single-service-2">
+                <div class="card border border-danger">
+                    <div class="card-body">
+                        <h4 class="text-primary text-left">Range Breakouts Scan</h4>
+                        <ul class="list-group text-left">
+                            <li class="list-group-item list-group-item-light"><a href="#">52 week breakout</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Nr7 - current day</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Range breakout with volume</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Narrow range 7 - nr7</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">52 week low</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Ohol+prb</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Nr7+nr4 with inside</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Technical indicators</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Yesterday's nr7 Stocks</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Nr7 ib - nr7 stocks with inside bar pattern by www.squareoff.in</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">003-nr7-9:30 buy scan</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Bollinger band squeeze</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Nr7-fut</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 col-md-4 single-service-2">
+                <div class="card border border-success">
+                    <div class="card-body">
+                        <h4 class="text-primary text-left">Bulish Scan</h4>
+                        <ul class="list-group text-left">
+                            <li class="list-group-item list-group-item-light"><a href="#">Bulish for next day</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Macd bearish or bulish crossover</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Bulish engulfing - strong</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Pure bulish trend stocks</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">2 days bulish by tomorrow</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Bulish engulfing pattern</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Golden Crossover</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Morning Star - bullish pattern</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Bullish-rsi-stochastic</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Bullish engulf</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Tradersway buy</a></li>
+                            <li class="list-group-item list-group-item-dark"><a href="#">Tss momentum long</a></li>
+
+                            <li class="list-group-item list-group-item-light"><a href="#">Pvc screener @ druzbeh</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>

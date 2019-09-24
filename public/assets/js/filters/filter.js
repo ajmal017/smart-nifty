c64b3f32a1c1aa3d43644b4af289d3d1836c38ee
gaurav_ranpara <ranparagaurav98@gmail.com>
2019-09-25 00:09:08 +0530

    Add More Filter in Screeners Page.

--- public/assets/js/filters/filter.js
+++ public/assets/js/filters/filter.js
@@ -145,3 +145,238 @@ $(document).on('change','#filterRule4Select',function(){
 ===========================================================================================================
 ===========================================================================================================
 **/
+
+/**
+===========================================================================================================
+										This is Rule 5 Script
+===========================================================================================================
+**/
+
+$(document).on('click','#filterRule5',function(){
+	$("#filterRule5").css('display','none');
+	$("#filterRule5Select").css('display','');
+});
+
+$(document).on('change','#filterRule5Select',function(){
+	var selectedValue = $(this).val();
+	if(selectedValue == "n_candles_ago"){
+		selectedValue = "-";
+		selectedValue += prompt("Enter Required offset, 1 offset equals 1 candle");
+	}
+	else if(selectedValue == "nth_candle_of_day"){
+		selectedValue = "=";
+		selectedValue += prompt("Enter candle of a day, starting with 1");
+	}
+	else if(selectedValue == "nth_last_candle_of_previous_day"){
+		selectedValue = "=-";
+		selectedValue += prompt("Enter candle of previous day, the last candle referred as -1");
+	}
+
+
+	$("#filterRuleChild5").text(selectedValue);
+	$("#filterRule5").css('display','');
+	$("#filterRule5Select").css('display','none');
+});
+
+
+/**
+===========================================================================================================
+===========================================================================================================
+**/
+
+
+
+/**
+===========================================================================================================
+										This is Rule 6 Script
+===========================================================================================================
+**/
+
+$(document).on('click','#filterRule6',function(){
+	$("#filterRule6").css('display','none');
+	$("#filterRule6Select").css('display','');
+});
+
+
+$(document).on('change','#filterRule6Select',function(){
+	var selectedValue = $("#filterRule6Select option:selected").html();
+	//var selectedValue = $(this).val();
+
+	if(selectedValue == "Latest(daily)" || selectedValue == "1 day ago" || selectedValue == "2 days ago" || selectedValue == "3 days ago" || selectedValue == "n days ago" || selectedValue == "Weekly" || selectedValue == "1 week ago" || selectedValue == "2 weeks ago" || selectedValue == "3 weeks ago" || selectedValue == "n weeks ago" || selectedValue == "Monthly" || selectedValue == "1 month ago" || selectedValue == "2 months ago" || selectedValue == "3 months ago" || selectedValue == "n moths ago")
+	{
+		if(selectedValue == "n days ago"){
+			selectedValue = prompt("Enter Required offset, 1 offset equals 1 candle");
+			selectedValue += " days ago";
+			$("#filterRule5").css('display','none');
+		}
+		else if(selectedValue == "n weeks ago"){
+			selectedValue = prompt("Enter Required offset, 1 offset equals 1 candle");
+			selectedValue += " weeks ago";
+		}
+		else if(selectedValue == "n moths ago"){
+			selectedValue = prompt("Enter Required offset, 1 offset equals 1 candle");
+			selectedValue += " moths ago";
+		}
+		else{
+			$("#filterRule5").css('display','none');
+		}
+	}
+	
+	else
+	{
+		$("#filterRule5").css('display','');	
+	}
+
+	$("#filterRuleChild6").text(selectedValue);
+	$("#filterRule6").css('display','');
+	$("#filterRule6Select").css('display','none');
+});
+
+/**
+===========================================================================================================
+===========================================================================================================
+**/
+
+/**
+===========================================================================================================
+										This is Rule 7 Script
+===========================================================================================================
+**/
+
+$(document).on('click','#filterRule7',function(){
+	$("#filterRule7").css('display','none');
+	$("#filterRule7Select").css('display','');
+});
+
+$(document).on('change','#filterRule7Select',function(){
+	var selectedValue = $(this).val();
+
+	$("#filterRuleChild7").text(selectedValue);
+	$("#filterRule7").css('display','');
+	$("#filterRule7Select").css('display','none');
+});
+
+
+/**
+===========================================================================================================
+===========================================================================================================
+**/
+
+/**
+===========================================================================================================
+										This is Rule 8 Script
+===========================================================================================================
+**/
+
+$(document).on('click','#filterRule8',function(){
+	$("#filterRule8").css('display','none');
+	$("#filterRule8Select").css('display','');
+});
+
+$(document).on('change','#filterRule8Select',function(){
+	var selectedValue = $(this).val();
+
+	$("#filterRuleChild8").text(selectedValue);
+	$("#filterRule8").css('display','');
+	$("#filterRule8Select").css('display','none');
+});
+
+
+/**
+===========================================================================================================
+===========================================================================================================
+**/
+
+
+
+
+/**
+===========================================================================================================
+							This is Rule-EQ 1 Script
+===========================================================================================================
+**/
+
+
+$(document).on('click','#filterRuleeq1',function(){
+	$("#filterRuleeq1").css('display','none');
+	$("#filterRuleeq1Select").css('display','');
+});
+
+
+$(document).on('change','#filterRuleeq1Select',function(){
+	var selectedValue = $(this).val();
+	if(selectedValue == "-n"){
+		selectedValue = "-";
+		selectedValue += prompt("Enter Required offset, 1 offset equals 1 candle");
+	}
+	else if(selectedValue == "=n"){
+		selectedValue = "=";
+		selectedValue += prompt("Enter candle of a day, starting with 1");
+	}
+	else if(selectedValue == "=-n"){
+		selectedValue = "=-";
+		selectedValue += prompt("Enter candle of previous day, the last candle referred as -1");
+	}
+	$("#filterRuleeqChild1").text(selectedValue);
+	$("#filterRuleeq1").css('display','');
+	$("#filterRuleeq1Select").css('display','none');
+});
+
+
+/**
+===========================================================================================================
+===========================================================================================================
+**/
+
+
+/**
+===========================================================================================================
+							This is Rule-EQ 2 Script
+===========================================================================================================
+**/
+
+$(document).on('click','#filterRuleeq2',function(){
+	$("#filterRuleeq2").css('display','none');
+	$("#filterRuleeq2Select").css('display','');
+});
+
+
+$(document).on('change','#filterRuleeq2Select',function(){
+	var selectedValue = $("#filterRuleeq2Select option:selected").html();
+	//var selectedValue = $(this).val();
+
+	if(selectedValue == "Latest(daily)" || selectedValue == "1 day ago" || selectedValue == "2 days ago" || selectedValue == "3 days ago" || selectedValue == "n days ago" || selectedValue == "Weekly" || selectedValue == "1 week ago" || selectedValue == "2 weeks ago" || selectedValue == "3 weeks ago" || selectedValue == "n weeks ago" || selectedValue == "Monthly" || selectedValue == "1 month ago" || selectedValue == "2 months ago" || selectedValue == "3 months ago" || selectedValue == "n moths ago")
+	{
+		if(selectedValue == "n days ago"){
+			selectedValue = prompt("Enter Required offset, 1 offset equals 1 candle");
+			selectedValue += " days ago";
+			$("#filterRuleeq2").css('display','none');
+		}
+		else if(selectedValue == "n weeks ago"){
+			selectedValue = prompt("Enter Required offset, 1 offset equals 1 candle");
+			selectedValue += " weeks ago";
+		}
+		else if(selectedValue == "n moths ago"){
+			selectedValue = prompt("Enter Required offset, 1 offset equals 1 candle");
+			selectedValue += " moths ago";
+		}
+		else{
+			$("#filterRuleeq1").css('display','none');
+		}
+	}
+	
+	else
+	{
+		$("#filterRuleeq1").css('display','');	
+	}
+
+	$("#filterRuleeqChild2").text(selectedValue);
+	$("#filterRuleeq2").css('display','');
+	$("#filterRuleeq2Select").css('display','none');
+});
+
+
+/**
+===========================================================================================================
+===========================================================================================================
+**/

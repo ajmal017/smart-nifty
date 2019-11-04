d3f78fce8cf2e1c0027045e03d00443aee414aaf
ranparagaurav <ranparagaurav98@gmail.com>
2019-11-04 21:11:11 +0530

    Change in Heading Equation.

--- db_equation_insert_script.php
+++ db_equation_insert_script.php
@@ -11,7 +11,12 @@ Top Loved
 		=============
 		b1:0,d1:15_minute,c1:close,a1:greater_than,b1:-1,d1:15_minute,c1:max,other:(,other:customtxt_20_,',b1:0,d1:15_minute,c1:close,arith,other:),arith==b1:0,d1:15_minute,c1:volume,a1:greater_than,b1:0,d1:15_minute,c1:sma,other:(,other:customtxt_volume_20,other:),arith
 
-		INSERT INTO screeners_equation(screener_name , screener_equation) VALUES('15-Minute-Stock-Breakouts','b1:0,d1:15_minute,c1:close,a1:greater_than,b1:-1,d1:15_minute,c1:max,other:(,other:customtxt_20_,',b1:0,d1:15_minute,c1:close,arith,other:),arith==b1:0,d1:15_minute,c1:volume,a1:greater_than,b1:0,d1:15_minute,c1:sma,other:(,other:customtxt_volume_20,other:),arith');
+		Insert Query
+		============
+		INSERT INTO screeners_equation(screener_name , heading_equation , screener_equation) VALUES(
+		'15-Minute-Stock-Breakouts',
+		'm1:passes,m2:all,m3:cash',
+		'b1:0,d1:15_minute,c1:close,a1:greater_than,b1:-1,d1:15_minute,c1:max,other:(,other:customtxt_20_,',b1:0,d1:15_minute,c1:close,arith,other:),arith==b1:0,d1:15_minute,c1:volume,a1:greater_than,b1:0,d1:15_minute,c1:sma,other:(,other:customtxt_volume_20,other:),arith');
 
 
 
@@ -19,4 +24,8 @@ Top Loved
 
 		Heading Equation
 		================
-		m1:passes,m2:all,m3:futures==
+		m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures
+
+		Main Equation
+		=============
+		

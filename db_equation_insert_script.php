f60ab942a36dc222b8195002b1f15b72ea67a962
gaurav_ranpara <ranparagaurav98@gmail.com>
2019-11-11 22:08:11 +0530

    no message

--- db_equation_insert_script.php
+++ db_equation_insert_script.php
@@ -1,3 +1,10 @@
+a1 = arithmatic filter
+b1 = numeric filter (e.g 0 Latest Candle)
+c1 = open filter (e.g open)
+d1 = timing filer (e.g 1 minute)
+other = ( OR ) OR customtxt_value1_value2
+
+
 Top Loved
 ---------
 	1) 15 Minute Stock Screener
@@ -9,7 +16,9 @@ Top Loved
 
 		Main Equation
 		=============
-		b1:0,d1:15_minute,c1:close,a1:greater_than,b1:-1,d1:15_minute,c1:max,other:(,other:customtxt_20_,',b1:0,d1:15_minute,c1:close,arith,other:),arith==b1:0,d1:15_minute,c1:volume,a1:greater_than,b1:0,d1:15_minute,c1:sma,other:(,other:customtxt_volume_20,other:),arith
+		b1:0,d1:15_minute,c1:close,a1:greater_than,b1:-1,d1:15_minute,c1:max,other:(,other:customtxt_20_,',b1:0,d1:15_minute,c1:close,arith,other:),arith
+		==
+		b1:0,d1:15_minute,c1:volume,a1:greater_than,b1:0,d1:15_minute,c1:sma,other:(,other:customtxt_volume_20,other:),arith
 
 		Insert Query
 		============
@@ -20,11 +29,11 @@ Top Loved
 
 
 
-	4) Builsh For Next Day
+	2) Short Term Breakouts
 
 		Heading Equation
 		================
-		m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures==m1:passes,m2:all,m3:futures
+		m1:passes,m2:all,m3:cash
 
 		Main Equation
 		=============
